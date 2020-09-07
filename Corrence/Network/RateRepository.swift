cf536cc3df277db9e59270a7692047d50343aa36
hikinit <hikinit@pm.me>
2020-09-07 11:06:24 +0700

    Add RateRepository

---
+++ Corrence/Network/RateRepository.swift
@@ -0,0 +1,19 @@
+//
+//  RateRepository.swift
+//  Corrence
+//
+//  Created by hikinit on 07/09/20.
+//
+
+import Foundation
+
+class RateRepository {
+  func fetch(base: String, completion: @escaping (Result<Currency, Error>) -> Void) {
+    let endpoint = APIEndpoint.latest(base: base)
+    let request = RateRequest(endpoint: endpoint)
+
+    request.load { result in
+      completion(result)
+    }
+  }
+}
---
+++ CorrenceTests/Network/RateRepositoryTests.swift
@@ -0,0 +1,40 @@
+//
+//  RateRepositoryTests.swift
+//  CorrenceTests
+//
+//  Created by hikinit on 07/09/20.
+//
+
+import XCTest
+
+class RateRepositoryTests: XCTestCase {
+  var sut: RateRepository!
+
+  override func setUp() {
+    super.setUp()
+    sut = RateRepository()
+  }
+
+  override func tearDown() {
+    sut = nil
+    super.tearDown()
+  }
+
+  func testFetch() {
+    let expectation = self.expectation(description: "Fetch rate")
+
+    sut.fetch(base: "USD") { result in
+      switch result {
+      case .success(let currency):
+        XCTAssertEqual(currency.base, "USD")
+      case .failure(let error):
+        XCTAssertThrowsError(error)
+      }
+
+      expectation.fulfill()
+    }
+
+    wait(for: [expectation], timeout: 2.0)
+  }
+
+}
