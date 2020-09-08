5c8b71b9f46d84a3f432bec8f011c1276e4a39e0
hikinit <hikinit@pm.me>
2020-09-08 14:57:01 +0700

    Add RateViewController and remove Main.storyboard

---
+++ Corrence/View/Controller/RateViewController.swift
@@ -0,0 +1,16 @@
+//
+//  RateViewController.swift
+//  Corrence
+//
+//  Created by hikinit on 08/09/20.
+//
+
+import UIKit
+
+class RateViewController: UIViewController {
+  @IBOutlet weak var tableView: UITableView!
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+}
