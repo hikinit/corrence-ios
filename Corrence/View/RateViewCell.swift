3502b5096c50d9f601e4af7e064ab1f392ab8be4
hikinit <hikinit@pm.me>
2020-09-12 13:53:56 +0700

    Embed SymbolView in to RateViewCell

--- Corrence/View/RateViewCell.swift
+++ Corrence/View/RateViewCell.swift
@@ -8,35 +8,12 @@
 import UIKit
 
 class RateViewCell: UITableViewCell, FromNIB {
-  @IBOutlet weak var currencyFlagImageView: UIImageView!
-  @IBOutlet weak var currencyNameLabel: UILabel!
   @IBOutlet weak var currencyValueLabel: UILabel!
   @IBOutlet weak var currencySymbolLabel: UILabel!
-
-  override func awakeFromNib() {
-    super.awakeFromNib()
-    setupUI()
-  }
-
+  @IBOutlet weak var symbolView: SymbolView!
+  
   func configure(with viewModel: RateViewCellModelType) {
-    currencyNameLabel.text = viewModel.output.currencyIsoCode
     currencyValueLabel.text = viewModel.output.currencyValue
     currencySymbolLabel.text = viewModel.output.currencySymbol
-
-    setupCountryFlag(viewModel.output.currencyIsoCode.lowercased())
-  }
-
-  private func setupCountryFlag(_ code: String) {
-    guard let image = UIImage(named: "flag/\(code)") else {
-      currencyFlagImageView.image = UIImage(named: "flag/generic")
-      return
-    }
-
-    currencyFlagImageView.image = image
-  }
-
-  private func setupUI() {
-    currencyFlagImageView.layer.cornerRadius = 4
-    currencyFlagImageView.clipsToBounds = true
   }
 }
